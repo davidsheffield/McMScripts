d29c64039f1e744eb7daa02e1c303ccef3e5ebde
David Sheffield <sheffield@physics.rutgers.edu>
2014-09-18 20:38:47 +0200

    Added checks for PWG and csv file's existance.

--- create.py
+++ create.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import sys
+import os.path
 import argparse
 
 def getArguments():
@@ -13,8 +15,30 @@ def getArguments():
     args_ = parser.parse_args()
     return args_
 
+def checkFile(file_):
+    if not os.path.isfile(file_):
+        print "Error: File %s does not exist." % file_
+        print "Exiting with status 1."
+        sys.exit(1)
+
+def checkPWG(pwg_):
+    pwg_list = ['B2G','BPH','BTW','EGM','EWK','EXO','FSQ','FWD','HCA','HIG','HIN','JME','L1T','MUO','QCD','SMP','SUS','TAU','TOP','TRK','TSG']
+    if pwg_ not in pwg_list:
+        print "Error: %s is not a recognized PWG." % pwg_
+        if pwg_ == 'XXX':
+            print "Change the default value for flag -p to your PWG."
+        sys.stdout.write("Options are:")
+        for iPWG in pwg_list:
+            sys.stdout.write(" ")
+            sys.stdout.write(iPWG)
+        sys.stdout.write("\n")
+        print "Exiting with status 2."
+        sys.exit(2)
+
 def main():
     args = getArguments()
+    checkPWG(args.pwg)
+    checkFile(args.file_in)
     print args.useDev
     print args.campaign
     print args.pwg
