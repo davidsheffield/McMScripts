df451583e1de154f22aa318cd98c93edda2ebba5
David Sheffield <sheffield@physics.rutgers.edu>
2014-11-22 18:37:23 +0100

    Added comments to getTimeSize.sh and have git ignore CSV files.

--- getTimeSize.sh
+++ getTimeSize.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+################################
+#
+# getTimeSize.sh
+#
+#  Script to extract the number of events, average CPU time, and file size
+#  from a job report and calculate the size per event in kilobytes.
+#
+#  author: David G. Sheffield (Rutgers)
+#
+################################
+
 if [ $# -ne 1 ]
   then
     echo "Provide job report. Usage:"
-    echo "./getTimeSize.sh CMSSW_X_X_X/src/test.xml"
+    echo "sh getTimeSize.sh job_report.xml"
     exit 1
 fi
 
@@ -14,5 +25,5 @@ time=`grep "AvgEventCPU" $1 | awk 'BEGIN{FS="\""}{print $4}'`
 echo "CPU Time [s]: "$time
 
 total_size=`grep "Timing-tstoragefile-write-totalMegabytes" $1 | awk 'BEGIN{FS="\""}{print $4}'`
-avg_size=`echo "$total_size*1024/$n" | bc -l`
+avg_size=`echo "$total_size*1024/$n" | bc -l` # Conver total size to kilobytes and calculate average per event
 echo "Size [kB]:    "$avg_size
