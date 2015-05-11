fdbcaeae280f638ddcbeb1e1d56a071214803ca8
David Sheffield <sheffield@physics.rutgers.edu>
2015-05-11 17:36:31 +0200

    Extract info from CSV file

--- testRequests.py
+++ testRequests.py
@@ -124,7 +124,163 @@ def createTest(compactPrepIDList, outputFile, nEvents):
         csvfile.writerow([req.getPrepId(), req.getJobID(), "", ""])
     return
 
+def exitDuplicateField(file_in_,field_):
+    print "Error: File %s contains multiple instances of the field %s" % (file_in_,field_)
+    print "Exiting with status 5."
+    sys.exit(5)
+
+def getFields(csvfile):
+    # List of indices for each field in CSV file
+    list = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+             -1, -1, -1, -1]
+    header = csv.reader(csvfile).next()
+    for ind, field in enumerate(header):
+        if field in ['Dataset name', 'Dataset Name', 'Dataset', 'dataset']:
+            #ensure no duplicate fields
+            if list[0] > -1: exitDuplicateField(file_in_, "Dataset name")
+            list[0] = ind
+        elif field in ['EOS', 'eos', 'Eos', 'MCDBID', 'mcdbid']:
+            if list[1] > -1: exitDuplicateField(file_in_, "EOS")
+            list[1] = ind
+        elif field in ['Cross section [pb]', 'Cross section',
+                       'Cross section (pb)', 'Cross Section',
+                       'Cross Section [pb]', 'Cross Section (pb)', 'CS',
+                       'CS [pb]', 'CS (pb)', 'Xsec', 'Xsec [pb]', 'Xsec (pb)']:
+            if list[2] > -1: exitDuplicateField(file_in_, "Cross section")
+            list[2] = ind
+        elif field in ['Total events', 'Total Events', 'Events', 'events',
+                       'total events', 'Number of Events']:
+            if list[3] > -1: exitDuplicateField(file_in_, "Total events")
+            list[3] = ind
+        elif field in ['Fragment name', 'Fragment Name',
+                       'Generator fragment name', 'Generator Fragment Name',
+                       'Fragment', 'fragment']:
+            if list[4] > -1: exitDuplicateField(file_in_, "Fragment name")
+            list[4] = ind
+        elif field in ['Time per event [s]', 'Time per event',
+                       'Time per event (s)', 'Time per Event',
+                       'Time per Event [s]', 'Time per Event (s)', 'Time',
+                       'Time [s]', 'Time (s)', 'time', 'time [s]', 'time (s)']:
+            if list[5] > -1: exitDuplicateField(file_in_, "Time per event [s]")
+            list[5] = ind
+        elif field in ['Size per event [kB]', 'Size per event',
+                       'Size per event (kB)', 'Size per Event',
+                       'Size per Event [kB]', 'Size per Event (kB)',
+                       'size', 'size [kB]', 'size (kB)']:
+            if list[6] > -1: exitDuplicateField(file_in_, "Size per event [kB]")
+            list[6] = ind
+        elif field in ['Tag', 'tag', 'Fragment Tag', 'Fragment tag',
+                       'fragment tag', 'sha', 'SHA', 'SHA-1', 'sha-1']:
+            if list[7] > -1: exitDuplicateField(file_in_, "Fragment tag")
+            list[7] = ind
+        elif field in ['Generator', 'generator']:
+            if list[8] > -1: exitDuplicateField(file_in_, "Generator")
+            list[8] = ind
+        elif field in ['Filter efficiency', 'FilterEfficiency',
+                       'filter efficiency']:
+            if list[9] > -1: exitDuplicateField(file_in_, "Filter efficiency")
+            list[9] = ind
+        elif field in ['Filter efficiency error', 'Filter Efficiency Error',
+                       'filter efficiency error']:
+            if list[10] > -1: exitDuplicateField(file_in_,
+                                                 "Filter efficiency error")
+            list[10] = ind
+        elif field in ['Match efficiency', 'Match Efficiency',
+                       'match efficiency']:
+            if list[11] > -1: exitDuplicateField(file_in_, "Match efficiency")
+            list[11] = ind
+        elif field in ['Match efficiency error', 'Match Efficiency Error',
+                       'match efficiency error']:
+            if list[12] > -1: exitDuplicateField(file_in_,
+                                                 "Match efficiency error")
+            list[12] = ind
+        elif field in ['PWG','pwg']:
+            if list[13] > -1: exitDuplicateField(file_in_, "PWG")
+            list[13] = ind
+        elif field in ['Campaign', 'campaign', 'Member of Campaign',
+                       'Member of campaign', 'member of campaign']:
+            if list[14] > -1: exitDuplicateField(file_in_, "Member of campaign")
+            list[14] = ind
+        elif field in ['PrepId', 'PrepID', 'PREPID', 'prepid']:
+            if list[15] > -1: exitDuplicateField(file_in_, "PrepId")
+            list[15] = ind
+        elif field in ['Sequences customise', 'Sequences customize']:
+            if list[16] > -1: exitDuplicateField(file_in_, "Sequences customise")
+            list[16] = ind
+        elif field in ['Process string', 'Process String']:
+            if list[17] > -1: exitDuplicateField(file_in_, "Process string")
+            list[17] = ind
+        elif field in ['Gridpack location', 'Gridpack']:
+            if list[18] > -1: exitDuplicateField(file_in_, "Gridpack location")
+            list[18] = ind
+        elif field in ['Gridpack cards URL', 'Cards URL',
+                       'Gridpack cards location', 'Cards location']:
+            if list[19] > -1: exitDuplicateField(file_in_, "Gridpack cards URL")
+            list[19] = ind
+        elif field in ['JobId']:
+            if list[20] > -1: exitDuplicateField(file_in_, "JobId")
+            list[20] = ind
+        else:
+            print "Error: The field %s is not valid." % (field)
+            print "Exiting with status 5."
+            sys.exit(6)
+
+    return list
+
+def fillFields(csvfile, fields):
+    requests = [] # List containing request objects
+    num_requests = 0
+    for row in csv.reader(csvfile):
+        num_requests += 1
+        tmpReq = Request()
+        if fields[0] > -1: tmpReq.setDataSetName(row[fields[0]])
+        if fields[1] > -1:
+            tmpReq.setMCDBID(row[fields[1]])
+        if fields[2] > -1:
+            tmpReq.setCS(row[fields[2]])
+        if fields[3] > -1: tmpReq.setEvts(row[fields[3]])
+        if fields[14] > -1:
+            campaign = row[fields[14]]
+            tmpReq.setCamp(campaign)
+        if fields[4] > -1: tmpReq.setFrag(formatFragment(row[fields[4]],campaign))
+        if fields[5] > -1 and row[fields[5]] != "":
+            tmpReq.setTime(row[fields[5]])
+        if fields[6] > -1 and row[fields[6]] != "":
+            tmpReq.setSize(row[fields[6]])
+        if fields[7] > -1: tmpReq.setTag(row[fields[7]])
+        if fields[8] > -1: tmpReq.setGen(row[fields[8]].split(" ")) # Multiple generators separated by spaces
+        if fields[9] > -1:
+            tmpReq.setFiltEff(row[fields[9]])
+        if fields[10] > -1:
+            tmpReq.setFiltEffErr(row[fields[10]])
+        if fields[11] > -1:
+            tmpReq.setMatchEff(row[fields[11]])
+        if fields[12] > -1:
+            tmpReq.setMatchEffErr(row[fields[12]])
+        if fields[13] > -1:
+            tmpReq.setPWG(row[fields[13]])
+        if fields[15] > -1:
+            tmpReq.setPrepId(row[fields[15]])
+        if fields[16] > -1:
+            tmpReq.setSequencesCustomise(row[fields[16]])
+        if fields[17] > -1:
+            tmpReq.setProcessString(row[fields[17]])
+        if fields[18] > -1:
+            if fields[19] > -1:
+                tmpReq.setMcMFrag(createLHEProducer(row[fields[18]],
+                                                    row[fields[19]]))
+            else:
+                tmpReq.setMcMFrag(createLHEProducer(row[fields[18]], ""))
+        if fields[20] > -1:
+            tmpReq.setJobID(row[fields[20]])
+        requests.append(tmpReq)
+    return requests, num_requests
+
 def extractTest(csvFile):
+    csvfile = open(csvFile, 'r') # Open CSV file
+    fields = getFields(csvfile)  # Get list of field indices
+    # Fill list of request objects from CSV file and get number of requests
+    requests, num_requests = fillFields(csvfile, fields)
     return
 
 def main():
