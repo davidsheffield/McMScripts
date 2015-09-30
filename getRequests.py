9a64cbca0e7aeaebb01114e8fbefc03460144416
David Sheffield <sheffield@physics.rutgers.edu>
2015-10-01 01:29:04 +0200

    Format output

--- getRequests.py
+++ getRequests.py
@@ -29,6 +29,8 @@ def getArguments():
     parser.add_argument('-n', action='store_true', dest='getNew', help='Only get requests with unmodified time and size per event.')
     parser.add_argument('-v', action='store_true', dest='getForValidation', help='Only get requests with positive time and size per event.')
     parser.add_argument('-c', action='store_true', dest='getChain', help='Return PrepID of chain.')
+    parser.add_argument('-f', dest='format', type=int, default=0,
+                        help='Format of output. 0 (default) = input for scripts, 1 = human-readable, 2 = HTML')
 
     args_ = parser.parse_args()
     return args_
@@ -67,13 +69,48 @@ def getPrepIDList(query_string, getNew, getForValidation, getChain):
     return out_list
 
 
-def printList(list):
+def isSequential(lastID, currentID):
+    last = lastID.split('-')
+    current = currentID.split('-')
+
+    if len(last) == 3 and len(current) == 3:
+        if last[0] == current[0] \
+                and last[1] == current[1] \
+                and int(last[2]) + 1 == int(current[2]):
+            return True
+    return False
+
+
+def printList(list, format):
+    arrow = "-"
+    comma = ","
+    if format == 1:
+        arrow = " ---> "
+        comma = ", "
+    elif format == 2:
+        arrow = " ---> "
+        comma = "<br>"
+
+    lastID = "FIRST"
+    print_last = False
+    last_index = len(list) - 1
     print ""
     for i, PrepID in enumerate(list):
-        if i == 0:
-            sys.stdout.write(PrepID)
+        if isSequential(lastID, PrepID):
+            if i < last_index:
+                print_last = True
+            else:
+                sys.stdout.write("{0}{1}".format(arrow, PrepID))
         else:
-            sys.stdout.write(",{0}".format(PrepID))
+            if print_last:
+                sys.stdout.write("{0}{1}{2}{3}".format(arrow, lastID, comma,
+                                                     PrepID))
+            elif i > 0:
+                sys.stdout.write("{0}{1}".format(comma, PrepID))
+            else:
+                sys.stdout.write("{0}".format(PrepID))
+            print_last = False
+        lastID = PrepID
     print "\n"
     return
 
@@ -83,7 +120,7 @@ def main():
 
     list = getPrepIDList(args.query, args.getNew, args.getForValidation,
                          args.getChain)
-    printList(list)
+    printList(list, args.format)
 
     return
 
