fa0c9a1151e2847bd776e2ba69ac2394cf2a7be0
David Sheffield <sheffield@physics.rutgers.edu>
2016-02-17 19:27:10 +0100

    Fields for all campaigns

--- database/addRequests.py
+++ database/addRequests.py
@@ -13,17 +13,21 @@
 import sqlite3
 import argparse
 import sys
+sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
+from rest import *
 
 
 def interrogate():
     loop = True
     while loop:
         process = raw_input("Process: ")
-        #requester = raw_input("Requester name: ")
-        #email = raw_input("Requester email: ")
+        requester = raw_input("Requester name: ")
+        email = raw_input("Requester email: ")
         tag = raw_input("Tag: ")
         events = raw_input ("Events: ")
         note = raw_input("Notes: ")
+        spreadsheet = raw_input("Spreadsheet: ")
+        campaign = raw_input("Campaign: ")
 
         yn = raw_input("Are these acceptable? [y/n] ")
         while True:
@@ -35,14 +39,18 @@ def interrogate():
             else:
                 yn = raw_input('Please enter "yes" or "no". ')
 
-    return [process, tag, events, note]
+    return [process, requester, email, tag, events, note, spreadsheet, campaign]
 
 
 def addRequestSet(answers):
+    mcm = restful(dev=False)
+    req_list = mcm.getA('requests', query='tags={0}&member_of_campaign={1}'.format(
+            answers[3], answers[7]))
+    multiplicity = len(req_list)
     conn = sqlite3.connect('EXO_MC_Requests.db')
     c = conn.cursor()
-    c.execute('INSERT INTO RequestSets VALUES(NULL, "{0}", NULL, NULL, "{1}", "{2}", "{3}", 0, 0, 0, 0, 0, 0, 0);'.\
-                  format(answers[0], answers[1], answers[2], answers[3]))
+    c.execute('INSERT INTO RequestSets (Process, Tag, Events, Notes, Spreadsheet, RequestMultiplicity) VALUES ("{0}", "{1}", {2}, "{3}", "{4}", {5});'.format(
+            answers[0], answers[3], answers[4], answers[5], answers[6], multiplicity))
     conn.commit()
     conn.close()
 
