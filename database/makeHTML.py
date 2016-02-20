36dca649eb2dc20701f3431ceea82f4c68041c2d
David Sheffield <sheffield@physics.rutgers.edu>
2016-02-20 20:37:21 +0100

    Add HTML file for analyzer

--- database/makeHTML.py
+++ database/makeHTML.py
@@ -103,8 +103,118 @@ def makeAnalyzerHTML():
     return
 
 
+def makeContactHTML():
+    fout = open('{0}contact.html'.format(mcmscripts_config.html_location), 'w')
+    status_name = [["LHE_New", "LHE_Validating", "LHE_Validated", "LHE_Defined",
+                    "LHE_Approved", "LHE_Submitted", "LHE_Done"],
+                   ["GS_New", "GS_Validating", "GS_Validated", "GS_Defined",
+                    "GS_Approved", "GS_Submitted", "GS_Done"],
+                   ["DR_New", "DR_Validating", "DR_Validated", "DR_Defined",
+                    "DR_Approved", "DR_Submitted", "DR_Done"],
+                   ["MiniAOD_New", "MiniAOD_Validating", "MiniAOD_Validated",
+                    "MiniAOD_Defined", "MiniAOD_Approved", "MiniAOD_Submitted",
+                    "MiniAOD_Done"],
+                   ["MiniAODv2_New", "MiniAODv2_Validating",
+                    "MiniAODv2_Validated", "MiniAODv2_Defined",
+                    "MiniAODv2_Approved", "MiniAODv2_Submitted",
+                    "MiniAODv2_Done"]]
+    campaigns = ["RunIIWinter15*LHE", "RunIISummer15GS", "RunIIFall15DR76",
+                 "RunIIFall15MiniAODv1", "RunIIFall15MiniAODv2"]
+    campaign_classes = ["lhe", "gs", "dr", "miniaod", "miniaodv2"]
+
+    fout.write("""\
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+    <title>Exotica MC Status for Contacts</title>
+
+    <link rel="stylesheet" type="text/css" href="global.css">
+
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+</head>
+
+<body>
+
+<div class="wrapper">
+<h1>Exotica MC</h1>
+
+<h2 class="campaign">7_6_X Campaign</h2>
+<p>(RunIIWinter15wmLHE/RunIIWinter15pLHE &rarr;) RunIISummer15GS &rarr; RunIIFall15DR &rarr; RunIIFall15MiniAODv1 &rarr; RunIIFall15MiniAODv2</p>
+<table>
+<tr class="table_header">
+    <th class="process">Process</th>
+    <th class="tag">Tag</th>
+    <th class="requester">Requester</th>
+    <th class="contact">Contact</th>
+    <th class="events">Events</th>
+    <th class="lhe">LHE</th>
+    <th class="gs">GS</th>
+    <th class="dr">DR</th>
+    <th class="miniaod">MiniAODv1</th>
+    <th class="miniaodv2">MiniAODv2</th>
+    <th class="spreadsheet">Spreadsheet</th>
+    <th class="notes">Notes</th>
+</tr>
+""")
+
+    conn = sqlite3.connect(mcmscripts_config.database_location)
+
+    c = conn.cursor()
+    c.execute("""SELECT Process, RequesterID, ContactID, Tag, Events, Notes, Spreadsheet, RequestMultiplicity,
+                 LHE_New, LHE_Validating, LHE_Validated, LHE_Defined, LHE_Approved, LHE_Submitted, LHE_Done,
+                 GS_New, GS_Validating, GS_Validated, GS_Defined, GS_Approved, GS_Submitted, GS_Done,
+                 DR_New, DR_Validating, DR_Validated, DR_Defined, DR_Approved, DR_Submitted, DR_Done,
+                 MiniAOD_New, MiniAOD_Validating, MiniAOD_Validated, MiniAOD_Defined, MiniAOD_Approved, MiniAOD_Submitted, MiniAOD_Done,
+                 MiniAODv2_New, MiniAODv2_Validating, MiniAODv2_Validated, MiniAODv2_Defined, MiniAODv2_Approved, MiniAODv2_Submitted, MiniAODv2_Done
+                 FROM RequestSets;""")
+    out = c.fetchall()
+
+    for request in out:
+        c.execute('SELECT Name, Email FROM Requesters WHERE RequesterID = {0};'.format(
+                request[1]))
+        requester = c.fetchall()
+        c.execute('SELECT DisplayName, Email FROM Contacts WHERE ContactID = {0};'.format(
+                request[2]))
+        contact = c.fetchall()
+        fout.write("""\
+<tr>
+    <td class="process">{0}</td>
+    <td class="tag">{1}</td>
+    <td class="requester"><a href="mailto:{2}">{3}</a></td>
+    <td class="contact">{4}</td>
+    <td class="events">{5}</td>
+""".format(request[0], request[3], requester[0][1], requester[0][0], contact[0][0], request[4]))
+        for i in range(len(status_name)):
+            fout.write("    <td class=\"{0}\"><a href=\"https://cms-pdmv.cern.ch/mcm/requests?tags={1}&member_of_campaign={2}&page=-1&shown=240518170655\" class=\"status\">{3}<br>{4}<br>{5}<br>{6}<br>{7}<br>{8}<br>{9}/{10}</a></td>\n".format(
+                    campaign_classes[i], request[3], campaigns[i],
+                    request[8 + i*7], request[9 + i*7], request[10 + i*7],
+                    request[11 + i*7], request[12 + i*7], request[13 + i*7],
+                    request[14 + i*7], request[7]))
+        fout.write("""\
+    <td class="spreadsheet"><a href="{0}">X</a></td>
+    <td class="notes">{1}</td>
+</tr>\n""".format(request[6], request[5]))
+    conn.close()
+
+    fout.write("""\
+</table>
+</div>
+
+</body>
+</html>
+""")
+    fout.close()
+
+    print "Generated contact page"
+
+    return
+
+
 def main():
     makeAnalyzerHTML()
+    makeContactHTML()
 
     return
 
